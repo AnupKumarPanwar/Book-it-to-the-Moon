cc69ca642f875914396fa5308d2d907497aa4d0f
Anup Panwar <mylnext@gmail.com>
2016-04-22 15:35:06 +0530

    new branch

--- moon_node/index.js
+++ moon_node/index.js
@@ -8,6 +8,11 @@ app.get('/getMoonData', function(req, res)
 	var d=new Date();
 	console.log(d.toLocaleTimeString());
 	var moonData=SunCalc.getMoonPosition(d,30.7398 , 76.782);
+	moonData.altitude=moonData.altitude*180.0/3.14;
+	moonData.azimuth=180.0+moonData.azimuth*180.0/3.14;
+
 	console.log(moonData);
 	res.send(moonData);
 })
+
+app.listen(8080);
